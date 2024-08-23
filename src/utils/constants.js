0783f10fb746408bee1549c8891c713c6c79a1aa
PranavPurkar <pranav.purkar07@gmail.com>
2024-08-24 00:10:44 +0530

    Added Youtube API

---
+++ src/utils/constants.js
@@ -0,0 +1,4 @@
+const GOOGLE_API_KEY ="AIzaSyAAnISE7IblkvziN5WbbSnN8H3983bUxyo";
+
+export const YOUTUBE_VIDEO_API = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=US&key="
+            +GOOGLE_API_KEY ;
